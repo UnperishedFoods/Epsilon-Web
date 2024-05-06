82cbd6e4c45bc8b08739c5b83943ba6740c20b62
UnperishedFoods <131722267+UnperishedFoods@users.noreply.github.com>
2024-05-06 21:35:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("sidebar").style.width = "30%";
+  document.getElementById("main").style.marginLeft = "30%";
+}
+
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
